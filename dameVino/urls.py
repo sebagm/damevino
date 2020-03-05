01222f501c679688abae65715c7bc168c87a1566
Sebastian Garcia <sebastian.garcia089@alu.uhu.es>
2020-03-05 12:54:43 +0100

    inicio

--- dameVino/urls.py
+++ dameVino/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gestionVinos.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', inicio),
 ]
