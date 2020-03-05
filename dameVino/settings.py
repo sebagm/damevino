a96618bac1d73e7ee44f2298d402b3323281c84e
Sebastian Garcia <sebastian.garcia089@alu.uhu.es>
2020-03-05 13:24:17 +0100

    templates

--- dameVino/settings.py
+++ dameVino/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dameVino.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['dameVino/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-eu'
 
 TIME_ZONE = 'UTC'
 
