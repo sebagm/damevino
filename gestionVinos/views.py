47691e2e180edc79903b38ee60a25a106b88dc46
Sebastian Garcia <sebastian.garcia089@alu.uhu.es>
2020-03-06 02:05:23 +0100

    base

--- gestionVinos/views.py
+++ gestionVinos/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def inicio(request):
     return render(request, "inicio.html")
+
+def recomendador(request):
+    return render(request, "recomendador.html")
+
+def vinoteca(request):
+    return render(request, "vinoteca.html")
+
+def detalles(request):
+    return render(request, "detalles.html")
+
+def contacto(request):
+    return render(request, "contacto.html")
