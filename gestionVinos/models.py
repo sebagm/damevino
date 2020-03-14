c757ef49da129ca6c9beef94461a6da6171f0335
Sebastian Garcia <sebastian.garcia089@alu.uhu.es>
2020-03-14 19:09:24 +0100

    commit basura

--- gestionVinos/models.py
+++ gestionVinos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Vinos(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=10)
+    anada = models.CharField(max_length=4)
+    denominacion = models.CharField(max_length=50)
+    bodega = models.CharField(max_length=50)
+    puntos = models.CharField(max_length=4)
+    maridaje = models.TextField()
+    vista = models.TextField()
+    nariz = models.TextField()
+    boca = models.TextField()
+    img = models.TextField()
