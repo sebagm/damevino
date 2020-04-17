972565efcee9292e5f220a312d12e482600ef6c4
Sebastian Garcia <sebastian.garcia089@alu.uhu.es>
2020-04-18 01:32:55 +0200

    Conf6

--- dameVino/settings.py
+++ dameVino/settings.py
@@ -126,9 +126,9 @@ STATICFILES_DIRS = [
     'dameVino/templates/static',
 ] """
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = 'dameVino/templates/static/'
+PROJECT_ROOT = '/dameVino/'
+STATIC_ROOT = '/dameVino/staticfiles/'
+STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
